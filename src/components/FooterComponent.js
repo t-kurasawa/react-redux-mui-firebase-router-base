c251b8a00b303df009a97df8335efcbfae857d42
t-kurasawa <>
2017-11-15 10:54:07 +0900

    add BottomNavigation

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,12 +1,42 @@
 import React from 'react'
 
+import FontIcon from 'material-ui/FontIcon';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+
 console.log("+++++ FooterComponent")
+
 class FooterComponent extends React.Component{
+  state = {
+    selectedIndex: 0,
+  };
+
+  select = (index) => this.setState({selectedIndex: index});
+
   render(){
     console.log("+++++ FooterComponent render")
     return(
-      <center><p>powered by <a href="https://github.com/t-kurasawa">t-kurasawa</a></p></center>
-    )
+      <Paper zDepth={1}>
+      <BottomNavigation selectedIndex={this.state.selectedIndex}>
+        <BottomNavigationItem
+          label="Home"
+          icon={<FontIcon className="material-icons">home</FontIcon>}
+          onClick={() => this.select(0)}
+        />
+        <BottomNavigationItem
+          label="Favorites"
+          icon={<FontIcon className="material-icons">favorite</FontIcon>}
+          onClick={() => this.select(1)}
+        />
+        <BottomNavigationItem
+          label="Nearby"
+          icon={<IconLocationOn />}
+          onClick={() => this.select(2)}
+        />
+      </BottomNavigation>
+    </Paper>
+  );    
   }
 }
 
